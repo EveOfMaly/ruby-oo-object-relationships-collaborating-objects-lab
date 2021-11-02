142b81e09ab9918b8ceabea6e4ccf13d1118a42c
EveOfMaly <john.a.kyeremeh@gmail.com>
2021-11-01 21:03:50 -0400

    completed assignment

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,46 +1,47 @@
 require 'pry'
 
-class Artist
-    attr_accessor :name
-    
-    @@all = []
+class Artist 
+  attr_accessor :name 
 
-    def initialize(name)
-        @name = name 
-        @@all << self 
-    end 
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end
 
-    def self.all
-        @@all 
+  def self.all 
+    @@all
+  end
+
+  def songs 
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def add_song(song)
+    song.artist = self 
+  end
+
+  def self.find_or_create_by_name(artist_name)
+    if (self.all.detect {|song| song.name == artist_name})
+      self.all.detect {|song| song.name == artist_name}
+    else
+      self.new(artist_name)
     end
+  end
 
 
-    def add_song(song)
-        song.artist = self 
+
+def print_songs
+    Song.all.each do |song|
+        if song.artist == self 
+          puts song.name
+        end 
     end
-
-    def songs
-        Song.all.select do |song|
-            song.artist == self 
-        end
-    end
-
-    def print_songs
-        Song.all.each do |song|
-            if song.artist == self 
-              puts song.name
-            end 
-        end
-    end
-
-
-    def self.find_or_create_by_name(artist_name)
-      if (self.all.detect {|song| song.name == artist_name})
-        self.all.detect {|song| song.name == artist_name}
-      else
-        self.new(artist_name)
-      end
-    end
-
-
 end
+
+
+
+
+
+end 
--- lib/song.rb
+++ lib/song.rb
@@ -1,38 +1,35 @@
-require 'pry'
 class Song
-    attr_accessor :artist, :name
+    attr_accessor :artist, :name 
+
 
     @@all = []
 
     def initialize(name)
-        @name = name
+        @name = name 
         @@all << self 
     end
 
-
     def self.all 
-        @@all
+        @@all 
     end
 
     def self.new_by_filename(file_name)
-        file_array = file_name.split("-")
-        file_artist = file_array[0].strip
-        file_name = file_array[1].strip
+        name = file_name.split("-")[1].strip
+        artist = file_name.split("-")[0].strip
 
-        new_instance_song = Song.new(file_name)
-        new_instance_song.artist_name = file_artist
-        new_instance_song 
+        new_instance = Song.new(name)
+        new_instance.artist = Artist.new(artist) 
+        new_instance
     end
 
-    def artist_name=(artist)
-        if (new_artist = Artist.all.detect {|i| i.name == artist})
-            self.artist = new_artist
-        else
-           new_artist =  Artist.new(artist)
-           self.artist = new_artist
-        end
+
+    def artist_name=(name)
+        new_instance = Artist.new(artist) 
+        new_instance.name = self 
+        self.artist = new_instance
+        new_instance
     end
+
+
 
 end
-
-
